b83bd789014b793cc7e86a16f03c3ad4569f50f3
yimje <imjy2@naver.com>
2023-08-09 11:53:16 +0900

    Usercontroller, ProductController 수정

--- src/main/java/com/jinnyshop5/controller/ProductController.java
+++ src/main/java/com/jinnyshop5/controller/ProductController.java
@@ -1,5 +1,10 @@
 package com.jinnyshop5.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
 public class ProductController {
 
 
--- src/main/java/com/jinnyshop5/controller/UserController.java
+++ src/main/java/com/jinnyshop5/controller/UserController.java
@@ -1,4 +1,58 @@
 package com.jinnyshop5.controller;
 
+import com.jinnyshop5.dto.UserFormDto;
+import com.jinnyshop5.entity.User;
+import com.jinnyshop5.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping(value = "/new")
+    public String userForm(Model model){
+        model.addAttribute("userFormDto", new UserFormDto());
+        return "user/userForm";
+    }
+
+    @PostMapping(value = "/new")
+    public String newUser(@Validated UserFormDto userFormDto, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            return "user/userForm";
+        }
+        try{
+            User user = User.createUser(userFormDto, passwordEncoder);
+            userService.saveUser(user);
+        }catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "user/userForm";
+        }
+
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/login")
+    public String loginUser(){
+        return "/user/userLoginForm";
+    }
+
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요!!");
+        return "user/userLoginForm";
+    }
 }
